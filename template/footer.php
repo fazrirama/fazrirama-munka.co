ac19b2c6924ef6f5c1522cf5c40d905093a260b1
fazrirama <fazri.ramadhan28@gmail.com>
2022-07-06 20:31:00 +0700

    first commit

---
+++ template/footer.php
@@ -0,0 +1,89 @@
+<footer>
+      <div class="footer-bottom mt-5">
+        <div class="container-fluid bg-dark">
+            <div class="row pl-3">
+                <div class="col-lg-3 col-md-3 col-sm-3 segment-one">
+                      <a href="index.html"><img src="image/logo/5.png" style="width: 100px; height: 100px"/></a>
+                  </div>
+  
+          <div class="col-lg-3 col-md-3 col-sm-3 segment-two">
+           <h5>Contact Us</h5>
+            <ul>
+               <li> Admin Munka.co at : </li>
+               <li><i class="fa fa-envelope"></i> hellomunka@gmail.com</li>
+               <li><a href=""><span class="fab fa-whatsapp" href="https://wa.wizard.id/ccdd17"></span> Whatsapp</a></li>   
+               <img src="image/logo/QR.png"/>
+              </ul>
+              
+          </div>
+  
+          <div class="col-lg-3 col-md-3 col-sm-3 segment-three">
+          <h5>Navigation</h5>
+          <ul>
+              <li><a href="index.html">Home </a><br>
+              <li><a href="index.html#judul-produk">Tops</a><br>
+              <li><a href="#">Lowers</a><br>
+              <li><a href="#">Outwears</a><br>
+              <li><a href="#">Bags</a><br>
+              <li><a href="#">Accesories</a><br>
+              <li><a href="#">Shoes</a><br>
+          </ul>
+          </div>
+  
+          <div class="col-lg-3 col-md-3 col-sm-3 .col-xs-15 text-left segment-four">
+              <h5>Follow Us</h5>
+              <li>Please Follow Us on our Social Media Profile in order to keep update.</li>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-youtube"></i></a>
+          </div>
+
+          <div class="col-lg-3 col-md-3 col-sm-3 segment-five">
+            <h5>Help</h5>
+            <ul>
+                <li><a href="#">About </a><br>
+                <li><a href="#">How To Order </a><br>
+                <li><a href="#">Return & Exchange Guide</a><br>
+                <li><a href="#">FAQ</a><br>
+                <li><a href="#">Term & Conditions</a><br>
+            </ul>
+            </div>
+  
+         </div>
+        </div>
+      </footer>
+      <div class="container-fluid bg-black">
+        <div class="row">
+             <!-- Copyright -->
+              <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0); color: azure; font-size: 16px;">
+                © 2022 Copyright
+                <a class="text-white" style="font-size: 16px;" href="index.html"><span style="color: #dd7c40;">Munka</span>.co</a>
+              </div>
+            <!-- Copyright -->
+        </div>
+      </div>
+
+      <!--footer-->
+   
+      
+      
+ 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script> //<![CDATA[
+      /* Sticky Header. Auto hide on scroll bottom, show on scroll top.  */
+      var prevScrollpos = window.pageYOffset;
+      window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+          document.getElementById("header").classList.remove('show');
+        }
+        else { document.getElementById("header").classList.add('show');
+        }
+        prevScrollpos = currentScrollPos;
+      }
+      //]]>
+  </script>
+  </body>
+
+  </html>
