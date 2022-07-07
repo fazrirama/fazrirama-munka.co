44005bcc0699a289e812cf88fb91ae61fe00d2a5
fazrirama <fazri.ramadhan28@gmail.com>
2022-07-07 08:18:48 +0700

    update file

---
+++ template/navbar.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
+    <title>Munka.co - Fashion Shop for Women</title>
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!--fontawesome--> 
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!--fontgoogle-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!--MY CSS-->
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+
+   <!--START-->
+  <body>
+    <link rel="shortcut icon" href="image/logo/revlogomunka2.png">
+    
+    
+    <header>
+    <!--First Navbar-->
+    <nav class="navbar-one flex">
+      <div class="left flex">
+        <a class="navbar-brand" href="index.html">
+          <div class="logo">
+          <img src="image/logo/3.png" style="margin: 0; width:117px "; />
+          </div>
+        </a>
+          <div class="user dropdown" aria-labelledby="navbarDropdown">
+             <span><i class="fa fa-user"></i> My Account</span>
+              <div class="isi-dropdown">
+                <a href="login.html"><i class="fa fa-sign-in"></i> Sign In</a><br>
+                <a href="#"><i class="fa fa-user-plus"></i> Register</a><br>
+                <hr>
+                <a href="#"><i class="fa fa-check"></i> Payment Confirmation</a><br>
+                <a href="#"><i class="fa fa-search"></i> Track Order</a><br>
+              </div>
+          </div>
+          <div class="cart">
+             <a href="cart.html"><span style="color: #f1b996"><i class="fa fa-cart-shopping"></i> Cart (0) </span></a>
+          </div>
+            <div class="wish dropdown">
+              <span><i class="fa fa-heart"></i> Wishlist</span>
+              <div class="isi-dropdown" style="width:220px; padding: 8px; padding-left: 23px;">
+                <p></i> You don't Have Any Wishlist Yet</p>
+              </div>  
+          </div>
+          <div class="co">
+              <span><i class="fa fa-cash-register"></i> Checkout</span>
+          </div>
+      </div>
+        <div class="right flex">
+            <div class="search-box">
+              <div class="container-1">
+                  <input class="search-txt" type="text" name="" placeholder="Search Product.." />
+                  <a class="search-btn" href="#"></a>
+                  <i class="fa fa-search"></i>
+              </div>
+            </div>
+        <div class="language">
+            <a><span> Language  <img src="image/vector/us.png" style="margin-right: 25px; width:12px; height:10px;"></span></a>
+        </div>
+        <div class="FAQ">
+          <span>FAQ</span>
+      </div>
+      </div>
+    </div>
+  </nav>    
+
+    <!--END CLEAR-->
+
+     <!--Second navbar-->
+     <div id="header">
+      <nav class="navbar navbar-expand-lg fixed">
+        <div class="container">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html"><span style="color: #e29a57">Home</span></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.html#judul-produk">Tops</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="#">Lowers</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="#">Outerwears</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="#">Bags</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="#">Accesories</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="#">Shoes</span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <br><br>
+  <!--End Navbar-->
+</header>
